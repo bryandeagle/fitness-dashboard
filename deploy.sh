55992d0850d2f1f96b0dea1d0bd85f4e05af7c98
Bryan Deagle <bryan.deagle@ni.com>
2020-11-30 23:34:15 -0600

    daemon mode

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 docker rm -f fitness
 docker build -t deagle/fitness:stable .
-docker run --init --name="fitness" --restart always -p 5100:8080 deagle/fitness:stable
+docker run --init --name="fitness" --restart always -d -p 5100:8080 deagle/fitness:stable
