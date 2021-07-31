4e2553b072c89735f54ea425eb152d7193902e08
bryandeagle <bryan@dea.gl>
2021-07-31 15:15:49 -0500

    two rows

--- app/app.py
+++ app/app.py
@@ -84,21 +84,6 @@ def layout():
                 dbc.Col(
                     html.Div(
                         dcc.Graph(
-                            id='body-weight',
-                            figure=px.line(df.tail(30),
-                                           x='Date',
-                                           y='Weight',
-                                           title='Body Weight (Lbs)',
-                                           line_shape='spline')
-                        ),
-                        className='card p-2'
-                    ),
-                    lg=6,
-                    className='mb-2'
-                ),
-                dbc.Col(
-                    html.Div(
-                        dcc.Graph(
                             id='weight-delta',
                             figure=px.bar(df.tail(30),
                                           x='Date',
@@ -107,8 +92,8 @@ def layout():
                         ),
                         className='card p-2'
                     ),
-                    lg=6,
-                    className='mb-2'
+                    className='mb-2',
+                    width=12
                 )
                 ],
                 className='gx-2'
