7ff2cdccb4efdc93268c2ae4fffcca4dea6f4204
bryandeagle <bryan@dea.gl>
2021-07-31 12:54:50 -0500

    make config dir

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,7 @@
 docker rm -f fitness
 docker build -t deagle/fitness:stable .
+mkdir -p /opt/fitness/config
+cp app/config.json /opt/fitness/config/
 docker run -d \
     --init \
     --volume /opt/fitness/config:/config
